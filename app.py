99dfbda052e37d6e97e69f5088bd2a260b72cd34
Gilbert Traczyk <traczyk.gilbert@gmail.com>
2022-11-01 21:56:08 -0400

    edited base sight layout

--- app.py
+++ app.py
@@ -4,4 +4,21 @@ app = Flask(__name__)
 
 @app.get('/')
 def index():
-    pass
+    return render_template('home_page.html')
+
+@app.get('/login')
+def index():
+    return render_template('login.html')
+
+@app.get('/home')
+def index():
+    return render_template('home_page.html')
+
+@app.get('/login/create')
+def index():
+    return render_template('create_account.html')
+
+@app.get('/create/post')
+def index():
+    return render_template('create_new_post.html')
+
