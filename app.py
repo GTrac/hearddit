70136b7ef8392291a8c1d03722a1c1b34de4b919
gtraczyk <traczyk.gilbert@gmail.com>
2022-11-04 13:23:26 -0400

    small fix

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from models import db, users, subh, posts
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'postgresql://postgres:HeardditAdminPassword@winhost:5432/hearddit'
+    'postgresql://postgres:HeardditAdminPassword@localhost:5432/hearddit'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 with app.app_context():
     db.create_all()
 
-new_user = users(user_name='test', user_password='test_password')
+test_new_user = users(user_name='test', user_password='test_password')
 
 @app.get('/')
 def index():
