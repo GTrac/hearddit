8a101ec510e816c388bce953847f95a4c9712845
gtraczyk <traczyk.gilbert@gmail.com>
2022-11-04 13:21:45 -0400

    created SQLAlchemy framework

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, redirect, render_template, request
-from models import db
+from models import db, users, subh, posts
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'postgresql://postgres:HeardditAdminPassword@localhost:5432/hearddit'
+    'postgresql://postgres:HeardditAdminPassword@winhost:5432/hearddit'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+new_user = users(user_name='test', user_password='test_password')
 
 @app.get('/')
 def index():
--- models.py
+++ models.py
@@ -1,3 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class users(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(255), nullable=False)
+    user_password = db.Column(db.String(255), nullable=False)
+
+class subh(db.Model):
+    sub_id = db.Column(db.Integer, primary_key=True)
+    sub_name = db.Column(db.String(20), nullable=False)
+    sub_post = db.Column(db.String(255), db.ForeignKey('posts.post_id'), nullable=True)
+    post = db.relationship('posts', backref='subs', lazy=True)
+    sub_post = db.Column(db.String(255), db.ForeignKey('users.user_id'), nullable=True)
+    user = db.relationship('users', backref='subs', lazy=True)
+
+class posts(db.Model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    post_title = db.Column(db.String(255), nullable=False)
+    post_link = db.Column(db.String(40000), nullable=True)
+    post_text = db.Column(db.String(40000), nullable=True)
+    post_title = db.Column(db.String(255), nullable=False)
+    post_sub = db.Column(db.String(20), db.ForeignKey('subh.sub_id'), nullable=False)
+    post_user = db.Column(db.String(20), db.ForeignKey('users.user_id'), nullable=False)
+    user = db.relationship('users', backref='posts_made', lazy=True)
