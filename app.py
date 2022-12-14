32f76bd04a6828b88107cbd0d80b72ff553ba418
Sean <50036097+sean-coyle@users.noreply.github.com>
2022-12-14 01:18:23 -0500

    fixed what if user isnt a spotify user and pastes a link stead of searching

--- app.py
+++ app.py
@@ -103,8 +103,10 @@ def create_post():
             track_id = tracks['tracks']['items'][0]['id']
         else:
             track_id = post_link[post_link.rfind('/'):].split('/',1)[1].split('?',1)[0]
-            
 
+    if post_link.startswith('https'):
+        track_id = post_link[post_link.rfind('/'):].split('/',1)[1].split('?',1)[0]         
+        print(track_id)
     if post_title == ' ' or post_text == '':
         abort(400)
     new_post = posts(com_id=com_id, post_title=post_title, post_link=post_link, post_text=post_text, post_rating=1, track_id = track_id)
